8d0ddc3788e579710ea307e9033f2cdd46137a12
okpyo12 <39540790+okpyo12@users.noreply.github.com>
2019-09-26 10:35:10 +0900

    CalenderMainActivity

---
+++ activity/CalenderMainActivity.java
@@ -0,0 +1,55 @@
+package com.cookandroid.mobileprogramming;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    int mYear, mMonth, mDay;
+    TextView mTxtDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_1);
+        mTxtDate = (TextView)findViewById(R.id.txtdate);
+
+        Calendar cal = new GregorianCalendar();
+        mYear = cal.get(Calendar.YEAR);
+        mMonth = cal.get(Calendar.MONTH);
+        mDay = cal.get(Calendar.DAY_OF_MONTH);
+        UpdateNow();
+
+    }
+
+    public void OnClickbtn(View v){
+        switch(v.getId()){
+            case R.id.btnchangedate:
+                new DatePickerDialog(MainActivity.this, mDateSetListener, mYear, mMonth, mDay).show();
+                break;
+        }
+
+    }
+
+    DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    UpdateNow();
+                }
+            };
+
+
+
+    void UpdateNow(){
+        mTxtDate.setText(String.format("%d/%d/%d", mYear, mMonth + 1, mDay));
+    }
+}
