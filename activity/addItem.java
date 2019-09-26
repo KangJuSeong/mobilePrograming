e262aee7dacefdf1b06f98ee16e63a07e53f541e
KangJuSeong <kjs@kookmin.ac.kr>
2019-09-26 13:18:05 +0900

    add textView

--- activity/addItem.java
+++ activity/addItem.java
@@ -9,24 +9,37 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class addItem extends AppCompatActivity {
-    EditText value;
-    Button btn2;
-    String str;
+    Button btn;
+    EditText id,name,age,gender;
+    String str_id,str_name,str_age,str_gender;
+    private DatabaseReference mPostReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.additem);
-        value = findViewById(R.id.value);
-        btn2 = findViewById(R.id.btn2);
+        id = findViewById(R.id.Id);
+        name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.gender);
+        btn = findViewById(R.id.btn);
 
-        btn2.setClickable(true);
-        btn2.setOnClickListener(new View.OnClickListener() {
+
+        btn.setClickable(true);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(addItem.this,RecyclerViewMainActivity.class);
-                str=value.getText().toString();
-                intent.putExtra("key",str);
+                str_id=id.getText().toString();
+                str_name=name.getText().toString();
+                str_age=age.getText().toString();
+                str_gender=gender.getText().toString();
+                intent.putExtra("ID",str_id);
+                intent.putExtra("NAME",str_name);
+                intent.putExtra("AGE",str_age);
+                intent.putExtra("GENDER",str_gender);
                 setResult(RESULT_OK,intent);
                 finish();
             }
